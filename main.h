06d022335c085633384c38ed8708d965782cbdfe
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-23 22:54:56 +0000

    Update main.h

--- main.h
+++ main.h
@@ -2,10 +2,12 @@
 #define MAIN_H
 
 #include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_str(va_list args);
+int print_percent(va_list args);
+int print_int(va_list args);
 
 #endif /* MAIN_H */
