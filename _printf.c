a48a62e44d19988a547ad9045ae632a71c46ea85
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-25 22:20:45 +0000

    printf

--- _printf.c
+++ _printf.c
@@ -104,6 +104,10 @@ int _printf(const char *format, ...)
 					count += print_unsigned(va_arg(args, unsigned int), 16, 1);
 					break;
 
+				case 'S':
+					count += print_string(va_arg(args, char *));
+					break;
+
 				case '%':
 					count += _putchar('%');
 					break;
@@ -147,38 +151,37 @@ int print_integer(int n)
 }
 
 /**
- * print_unsigned - prints an unsigned int to stdout
- * @n: the unsigned int to print
- * @base: the base to use for printing (8 for octal, 10 for decimal, 16 for hex)
- * @uppercase: 1 if the letters in hex should be uppercase, 0 if they should be lowercase
+ *print_unsigned - prints an unsigned int to stdout
+ *@n: the unsigned int to print
+ *@base: the base to use for printing (8 for octal, 10 for decimal, 16 for hex)
+ *@uppercase: 1 if the letters in hex should be uppercase, 0 if they should be lowercase
  *
- * Return: the number of characters printed
+ *Return: the number of characters printed
  */
 int print_unsigned(unsigned int n, int base, int uppercase)
 {
-        int count = 0, i = 0;
-        char buf[1024];
-        char *digits = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
+	int count = 0, i = 0;
+	char buf[1024];
+	char *digits = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
 
-        if (n == 0)
-        {
-                return (_putchar('0'));
-        }
+	if (n == 0)
+	{
+		return (_putchar('0'));
+	}
 
-        while (n != 0)
-        {
-                buf[i++] = digits[n % base];
-                n /= base;
-        }
+	while (n != 0)
+	{
+		buf[i++] = digits[n % base];
+		n /= base;
+	}
 
-        while (i > 0)
-        {
-                count += _putchar(buf[--i]);
-        }
+	while (i > 0)
+	{
+		count += _putchar(buf[--i]);
+	}
 
-        return (count);
+	return (count);
 }
-
 #include "main.h"
 
 /*
