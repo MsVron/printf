0410f4035abb7a462984f15955f4ed86ea7b092b
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-25 14:12:45 +0000

    main lib

--- main.h
+++ main.h
@@ -1,8 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 
-
-#endif /*MAIN_H */
+#endif /* MAIN_H */
