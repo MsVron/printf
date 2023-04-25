80fa21c0bebe414faf7953e1522050351aa4aa3f
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-25 22:07:04 +0000

    main lib

--- main.h
+++ main.h
@@ -9,5 +9,6 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 int print_integer(int n);
 int _print_binary(unsigned int n);
+int print_unsigned(unsigned int n, int base, int uppercase);
 
 #endif /* MAIN_H */
