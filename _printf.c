d0b2ec6a107e49b2f756dc050565d5822f89cf5d
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-25 21:52:20 +0000

    printf

--- _printf.c
+++ _printf.c
@@ -8,14 +8,30 @@
  */
 
 int _putchar(char c)
-
 {
 	return (write(1, &c, 1));
-
 }
 
 /*
+ *_print_binary - prints an unsigned int in binary
+ *@n: the unsigned int to print
+ *
+ *Return: the number of characters printed
+ */
 
+int _print_binary(unsigned int n)
+{
+	int count = 0;
+
+	if (n / 2)
+		count += _print_binary(n / 2);
+
+	count += _putchar(n % 2 + '0');
+
+	return (count);
+}
+
+/*
  *_printf - produces output according to a format
  *@format: character string containing zero or more directives
  *
@@ -24,76 +40,66 @@ int _putchar(char c)
  */
 
 int _printf(const char *format, ...)
-
 {
 	va_list args;
-
 	int count = 0;
 
 	va_start(args, format);
 
 	while (*format != '\0')
-
 	{
 		if (*format == '%')
-
 		{
-
 			format++; /*skip the % */
-
 			switch (*format)
-
 			{
-
 				case 'c':
-
 					count += _putchar(va_arg(args, int));
-
 					break;
 
 				case 's':
-
 					{
-
 						char *s = va_arg(args, char *);
-
 						if (s == NULL)
-
 							s = "(null)";
-
 						for (; *s != '\0'; s++)
-
 							count += _putchar(*s);
-
 						break;
 					}
 
 				case 'd':
 				case 'i':
+					{
+						int n = va_arg(args, int);
+						if (n < 0)
+						{
+							count += _putchar('-');
+							n = -n;
+						}
 
-					count += print_integer(va_arg(args, int));
+						count += print_integer(n);
+						break;
+					}
 
-					break;
+				case 'b':
+					{
+						unsigned int n = va_arg(args, unsigned int);
+						count += _print_binary(n);
+						break;
+					}
 
 				case '%':
-
 					count += _putchar('%');
-
 					break;
 
 				default:
-
 					count += _putchar('%');
-
 					count += _putchar(*format);
-
 					break;
 			}
 		}
 		else
-
 		{
-
 			count += _putchar(*format);
 		}
 
@@ -103,42 +109,23 @@ int _printf(const char *format, ...)
 	va_end(args);
 
 	return (count);
-
 }
 
 /*
-
  *print_integer - prints an integer to stdout
-
  *@n: the integer to print
-
  *
-
  *Return: the number of characters printed
-
  */
 
 int print_integer(int n)
-
 {
 	int count = 0;
 
-	if (n < 0)
-
-	{
-		count += _putchar('-');
-
-		n = -n;
-	}
-
 	if (n / 10)
-
-	{
 		count += print_integer(n / 10);
-	}
 
 	count += _putchar(n % 10 + '0');
 
 	return (count);
-
 }
