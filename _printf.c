e8981862d66d377b0518de0f217f1cde6b5ba691
Aya Berroukech <ayagym14@gmail.com>
2023-04-25 07:28:41 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,62 +1,144 @@
 #include "main.h"
 
-/**
- * _putchar - writes a character to stdout
- * @c: the character to print
+/*
+ *_putchar - writes a character to stdout
+ *@c: the character to print
  *
- * Return: the number of characters printed
+ *Return: the number of characters printed
  */
+
 int _putchar(char c)
+
 {
 	return (write(1, &c, 1));
+
 }
 
-/**
- * _printf - produces output according to a format
- * @format: character string containing zero or more directives
+/*
+
+ *_printf - produces output according to a format
+ *@format: character string containing zero or more directives
  *
- * Return: the number of characters printed (excluding the null byte used to end output to strings)
+ *Return: the number of characters printed
+ *(excluding the null byte used to end output to strings)
  */
+
 int _printf(const char *format, ...)
+
 {
 	va_list args;
+
 	int count = 0;
 
 	va_start(args, format);
+
 	while (*format != '\0')
+
 	{
 		if (*format == '%')
+
 		{
-			format++; /* skip the % */
+
+			format++; /*skip the % */
+
 			switch (*format)
+
 			{
+
 				case 'c':
+
 					count += _putchar(va_arg(args, int));
+
 					break;
+
 				case 's':
+
 					{
+
 						char *s = va_arg(args, char *);
+
 						if (s == NULL)
+
 							s = "(null)";
+
 						for (; *s != '\0'; s++)
+
 							count += _putchar(*s);
+
 						break;
 					}
-				case '%':
-					count += _putchar('%');
+
+				case 'd':
+				case 'i':
+
+					count += print_integer(va_arg(args, int));
+
 					break;
-				default:
+
+				case '%':
+
 					count += _putchar('%');
+
+					break;
+
+				default:
+
+					count += _putchar('%');
+
 					count += _putchar(*format);
+
 					break;
 			}
 		}
 		else
+
 		{
+
 			count += _putchar(*format);
 		}
+
 		format++;
 	}
+
 	va_end(args);
+
 	return (count);
+
+}
+
+/*
+
+ *print_integer - prints an integer to stdout
+
+ *@n: the integer to print
+
+ *
+
+ *Return: the number of characters printed
+
+ */
+
+int print_integer(int n)
+
+{
+	int count = 0;
+
+	if (n < 0)
+
+	{
+		count += _putchar('-');
+
+		n = -n;
+	}
+
+	if (n / 10)
+
+	{
+		count += print_integer(n / 10);
+	}
+
+	count += _putchar(n % 10 + '0');
+
+	return (count);
+
 }
