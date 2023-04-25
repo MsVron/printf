707f04dafe08f5cbe3b74da45f9976ebd2b69930
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-25 22:17:59 +0000

    printf

--- _printf.c
+++ _printf.c
@@ -146,24 +146,35 @@ int print_integer(int n)
 	return (count);
 }
 
-/*
- *print_unsigned - prints an unsigned int to stdout
- *@n: the unsigned int to print
- *@base: the base to use for printing (8 for octal, 10 for decimal, 16 for hex)
- *@uppercase: 1 if the letters in hex should be uppercase, 0 if they should be lowercase
+/**
+ * print_unsigned - prints an unsigned int to stdout
+ * @n: the unsigned int to print
+ * @base: the base to use for printing (8 for octal, 10 for decimal, 16 for hex)
+ * @uppercase: 1 if the letters in hex should be uppercase, 0 if they should be lowercase
  *
- *Return: the number of characters printed
+ * Return: the number of characters printed
  */
-
 int print_unsigned(unsigned int n, int base, int uppercase)
 {
-	int count = 0;
-	char *digits = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
+        int count = 0, i = 0;
+        char buf[1024];
+        char *digits = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
 
-	if (n / base)
-		count += print_unsigned(n / base, base, uppercase);
+        if (n == 0)
+        {
+                return (_putchar('0'));
+        }
 
-	count += _putchar(digits[n % base]);
+        while (n != 0)
+        {
+                buf[i++] = digits[n % base];
+                n /= base;
+        }
 
-	return (count);
+        while (i > 0)
+        {
+                count += _putchar(buf[--i]);
+        }
+
+        return (count);
 }
