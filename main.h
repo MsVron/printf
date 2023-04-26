fd1af233b125669c11ecce632c0166b96cbcfac8
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-26 23:13:28 +0000

    main lib

--- main.h
+++ main.h
@@ -10,9 +10,8 @@ int _printf(const char *format, ...);
 int print_integer(int n);
 int _print_binary(unsigned int n);
 int print_unsigned(unsigned int n, int base, int uppercase);
-int print_string(char *str);
+int print_string(va_list arg);
 int print_pointer(void *ptr);
 int handle_flags(char *flags, char specifier, va_list args);
-void print_buffer(char *buffer, int *count);
 
 #endif /* MAIN_H */
