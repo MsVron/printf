d7378391f6edb92fb9a75e8dd0a92ae8abb6dc7c
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-29 12:57:01 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -10,87 +10,70 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int printed_chars = 0;
+	int i = 0;
 
 	va_start(args, format);
-	printed_chars = print_string_format(format, args);
-	va_end(args);
-
-	return (printed_chars);
-}
-
-/**
- * print_string_format - prints formatted output to stdout with variadic arguments
- * @format: a string containing zero or more directives to write to stdout
- * @args: a va_list of variadic arguments
- *
- * Return: the number of characters printed (excluding the null byte used to end output to strings)
- */
-int print_string_format(const char *format, va_list args)
-{
-	int i = 0;
-	int printed_chars = 0;
 
 	while (format && format[i])
 	{
 		if (format[i] == '%')
 		{
 			i++;
-			printed_chars += print_directive(format[i], args);
+			switch (format[i])
+			{
+				case 'c':
+					printed_chars += _putchar(va_arg(args, int));
+					break;
+
+				case 's':
+					printed_chars += print_string(args);
+					break;
+
+				case '%':
+					printed_chars += _putchar('%');
+					break;
+
+				case 'd':
+				case 'i':
+					printed_chars += print_integer(va_arg(args, int));
+					break;
+
+				case 'b':
+					printed_chars += print_binary(va_arg(args, unsigned int));
+					break;
+
+				case 'u':
+					printed_chars += print_unsigned(va_arg(args, unsigned int), 10, 0);
+					break;
+
+				case 'o':
+					printed_chars += print_unsigned(va_arg(args, unsigned int), 8, 0);
+					break;
+
+				case 'x':
+					printed_chars += print_unsigned(va_arg(args, unsigned int), 16, 0);
+					break;
+
+				case 'X':
+					printed_chars += print_unsigned(va_arg(args, unsigned int), 16, 1);
+					break;
+
+				default:
+					_putchar('%');
+					_putchar(format[i]);
+					printed_chars += 2;
+					break;
+			}
 		}
 		else
 		{
-			_putchar(format[i]);
-			printed_chars++;
+			printed_chars += _putchar(format[i]);
 		}
+
 		i++;
 	}
 
+	va_end(args);
+
 	return (printed_chars);
-}
-
-/**
- * print_directive - prints the specified directive to stdout with variadic arguments
- * @directive: the directive to print to stdout
- * @args: a va_list of variadic arguments
- *
- * Return: the number of characters printed (excluding the null byte used to end output to strings)
- */
-int print_directive(char directive, va_list args)
-{
-	switch (directive)
-	{
-		case 'c':
-			return (_putchar(va_arg(args, int)));
-
-		case 's':
-			return (print_string(args));
-
-		case '%':
-			_putchar('%');
-			return (1);
-
-		case 'd':
-		case 'i':
-			return (print_integer(va_arg(args, int)));
-
-		case 'b':
-			return (print_binary(va_arg(args, unsigned int)));
-
-		case 'u':
-			return (print_unsigned(va_arg(args, unsigned int), 10, 0));
-
-		case 'o':
-			return (print_unsigned(va_arg(args, unsigned int), 8, 0));
-
-		case 'x':
-			return (print_unsigned(va_arg(args, unsigned int), 16, 0));
-
-		case 'X':
-			return (print_unsigned(va_arg(args, unsigned int), 16, 1));
-
-		default:
-			_putchar('%');
-			_putchar(directive);
-			return (2);
-	}
 }
