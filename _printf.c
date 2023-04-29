b899a24624ced2800217691dc0ff5dd4a5dfee84
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-29 12:17:47 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -102,66 +102,52 @@ int print_number(unsigned int n, unsigned int base, char *digits)
  *Return: the number of characters printed
  */
 
-int _printf(const char *format, ...)
+while (*format)
 {
-	va_list arg;
-	int count = 0;
-
-	va_start(arg, format);
-
-	while (*format)
+	if (*format == '%')
 	{
-		if (*format == '%')
-		{
-			format++;
-
-			if (*format == '\0')
-				break;
-
-			switch (*format)
-			{
-				case 'c':
-					count += print_char(arg);
-					break;
-				case 's':
-					count += print_string(arg);
-					break;
-				case 'd':
-					count += print_number(va_arg(arg, int), 10, "0123456789");
-					break;
-				case 'x':
-					count += print_number(va_arg(arg, unsigned int), 16, "0123456789abcdef");
-					break;
-				case 'X':
-					count += print_number(va_arg(arg, unsigned int), 16, "0123456789ABCDEF");
-					break;
-				case 'u':
-					count += print_unsigned(va_arg(arg, unsigned int), 10, 0);
-					break;
-				case '%':
-					_putchar('%');
-					count++;
-					break;
-				default:
-					_putchar('%');
-					_putchar(*format);
-					count += 2;
-					break;
-			}
-		}
-		else
-		{
-			_putchar(*format);
-			count++;
-		}
-
 		format++;
+
+		if (*format == '\0')
+			break;
+
+		switch (*format)
+		{
+			case 'c':
+				count += print_char(arg);
+				break;
+			case 's':
+				count += print_string(arg);
+				break;
+			case 'd':
+				count += print_number(va_arg(arg, int), 10, "0123456789");
+				break;
+			case 'x':
+				count += print_number(va_arg(arg, unsigned int), 16, "0123456789abcdef");
+				break;
+			case 'X':
+				count += print_number(va_arg(arg, unsigned int), 16, "0123456789ABCDEF");
+				break;
+			case '%':
+				_putchar('%');
+				count++;
+				break;
+			default:
+				_putchar('%');
+				_putchar(*format);
+				count += 2;
+				break;
+		}
+	}
+	else
+	{
+		_putchar(*format);
+		count++;
 	}
 
-	va_end(arg);
-
-	return (count);
+	format++;
 }
+
 /**
  *print_unsigned - prints an unsigned int to stdout
  *@n: the unsigned int to print
