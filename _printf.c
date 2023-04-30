a285959968717eb6cca983841d2547a0bb96cae8
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-30 17:20:45 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -9,73 +9,47 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int printed_chars = 0;
-	int i = 0;
-
-	if (format == NULL)
-		return (-1);
+	int i, printed_chars = 0;
 
 	va_start(args, format);
 
-	while (format[i])
+	for (i = 0; format && format[i]; i++)
 	{
-		if (format[i] == '%')
+		if (format[i] != '%')
 		{
-			i++;
-			if (format[i] == '\0')
-				return (-1);
-			switch (format[i])
-			{
-				case 'c':
-					printed_chars += _putchar(va_arg(args, int));
-					break;
-
-				case 's':
-					printed_chars += print_string(args);
-					break;
-
-				case '%':
-					printed_chars += _putchar('%');
-					break;
-
-				case 'd':
-				case 'i':
-					printed_chars += print_integer((long int) va_arg(args, int));
-					break;
-
-				case 'b':
-					printed_chars += print_binary(va_arg(args, unsigned int));
-					break;
-
-				case 'u':
-					printed_chars += print_unsigned(va_arg(args, unsigned int), 10, 0);
-					break;
-
-				case 'o':
-					printed_chars += print_unsigned(va_arg(args, unsigned int), 8, 0);
-					break;
-
-				case 'x':
-					printed_chars += print_unsigned(va_arg(args, unsigned int), 16, 0);
-					break;
-
-				case 'X':
-					printed_chars += print_unsigned(va_arg(args, unsigned int), 16, 1);
-					break;
-
-				default:
-					_putchar('%');
-					_putchar(format[i]);
-					printed_chars += 2;
-					break;
-			}
-		}
-		else
-		{
-			printed_chars += _putchar(format[i]);
+			_putchar(format[i]);
+			printed_chars++;
+			continue;
 		}
 
-		i++;
+		switch (format[++i])
+		{
+			case 'c':
+				_putchar(va_arg(args, int));
+				printed_chars++;
+				break;
+			case 's':
+				printed_chars += print_string(va_arg(args, char *));
+				break;
+			case 'd':
+			case 'i':
+				printed_chars += print_integer(va_arg(args, int));
+				break;
+			case 'b':
+				printed_chars += print_binary(va_arg(args, unsigned int));
+				break;
+			case 'u':
+				printed_chars += print_unsigned(va_arg(args, unsigned int));
+				break;
+			case '%':
+				_putchar('%');
+				printed_chars++;
+				break;
+			default:
+				_putchar('%');
+				_putchar(format[i]);
+				printed_chars += 2;
+		}
 	}
 
 	va_end(args);
