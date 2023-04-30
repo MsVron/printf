39c19fa5b80d3594cd3de9b0ce8315c38e20b830
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-30 17:04:15 +0100

    Update print_integer.c

--- print_integer.c
+++ print_integer.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  *print_integer - Prints an integer to stdout
@@ -9,6 +10,15 @@
 int print_integer(int n)
 {
 	int count = 0;
+	int is_negative = 0;
+
+	if (n == INT_MIN)
+	{
+		/*Handle INT_MIN separately */
+		count += _putchar('-');
+		n = -(n + 1); /*Avoid overflow when converting to positive */
+		is_negative = 1;
+	}
 
 	if (n < 0)
 	{
@@ -20,6 +30,12 @@ int print_integer(int n)
 		count += print_integer(n / 10);
 
 	count += _putchar((n % 10) + '0');
+
+	if (is_negative)
+	{
+		/*Add 1 to the final count to account for the extra digit */
+		count++;
+	}
 
 	return (count);
 }
