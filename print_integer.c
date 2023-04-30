914f5ab123c36bde04c63fb9539607798b0326df
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-30 22:36:33 +0100

    Update print_integer.c

--- print_integer.c
+++ print_integer.c
@@ -7,32 +7,30 @@
  *
  *Return: The number of digits printed
  */
-
 int print_integer(int n)
 {
-	int count = 0;
-	int is_negative = 0;
+    int count = 0;
+    int is_negative = 0;
 
-	if (n == INT_MIN)
-	{
-		/*Handle INT_MIN separately */
-		count += _putchar('-');
-		n = -(n + 1); /*Avoid overflow when converting to positive */
-		is_negative = 1;
-	}
+    if (n == INT_MIN)
+    {
+        /*Handle INT_MIN separately */
+        n = -(n + 1); /*Avoid overflow when converting to positive */
+        is_negative = 1;
+    }
 
-	if (n < 0)
-	{
-		count += _putchar('-');
-		n = -n;
-	}
+    if (n < 0)
+    {
+        count += _putchar('-');
+        n = -n;
+    }
 
-	if (n / 10 != 0)
-		count += print_integer(n / 10);
-	else if (is_negative)
-		count++; /*Account for extra digit */
+    if (n / 10 != 0)
+        count += print_integer(n / 10);
+    else if (is_negative)
+        count++; /*Account for extra digit */
 
-	count += _putchar((n % 10) + '0');
+    count += _putchar((n % 10) + '0');
 
-	return (count);
+    return (count);
 }
