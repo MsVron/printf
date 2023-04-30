d556652250b9dee0f5d7a4c3b989d1d68b144381
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-30 22:39:06 +0100

    Update print_integer.c

--- print_integer.c
+++ print_integer.c
@@ -7,16 +7,22 @@
  *
  *Return: The number of digits printed
  */
+/**
+ *print_integer - Prints an integer to stdout
+ *@n: The integer to print
+ *
+ *Return: The number of digits printed
+ */
 int print_integer(int n)
 {
     int count = 0;
-    int is_negative = 0;
 
     if (n == INT_MIN)
     {
-        /*Handle INT_MIN separately */
-        n = -(n + 1); /*Avoid overflow when converting to positive */
-        is_negative = 1;
+        /* Handle INT_MIN separately */
+        count += _putchar('-');
+        count += _putchar('2');
+        n = (n % 1000000000) * -1; /* Get the positive value of the last 9 digits */
     }
 
     if (n < 0)
@@ -27,8 +33,6 @@ int print_integer(int n)
 
     if (n / 10 != 0)
         count += print_integer(n / 10);
-    else if (is_negative)
-        count++; /*Account for extra digit */
 
     count += _putchar((n % 10) + '0');
 
