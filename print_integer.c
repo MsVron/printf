287a86f19da7af5915544f2da74005a655621f9f
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-30 17:18:59 +0100

    Update print_integer.c

--- print_integer.c
+++ print_integer.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <limits.h>
 
-//**
+/**
  *print_integer - Prints an integer to stdout
  *@n: The integer to print
  *
  *Return: The number of digits printed
  */
+
 int print_integer(int n)
 {
 	int count = 0;
