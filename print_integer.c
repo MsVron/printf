3d4840566de9077a0a5656fb4d3f26e983cf53f6
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-30 22:31:50 +0100

    Update print_integer.c

--- print_integer.c
+++ print_integer.c
@@ -1,21 +1,21 @@
-#include "main.h"
-#include <limits.h>
-
 /**
- * print_integer - Prints an integer to stdout
- * @n: The integer to print
+ *print_integer - Prints an integer to stdout
+ *@n: The integer to print
  *
- * Return: The number of digits printed
+ *Return: The number of digits printed
  */
+
 int print_integer(int n)
 {
 	int count = 0;
+	int is_negative = 0;
 
 	if (n == INT_MIN)
 	{
+		/*Handle INT_MIN separately */
 		count += _putchar('-');
-		count += _putchar('2');
-		n = 147483648; /* Absolute value of INT_MIN */
+		n = -(n + 1); /*Avoid overflow when converting to positive */
+		is_negative = 1;
 	}
 
 	if (n < 0)
@@ -26,6 +26,8 @@ int print_integer(int n)
 
 	if (n / 10 != 0)
 		count += print_integer(n / 10);
+	else if (is_negative)
+		count++; /*Account for extra digit */
 
 	count += _putchar((n % 10) + '0');
 
