f3b576a9e51cea707685a82537fc1e6af05fea05
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-30 16:58:54 +0100

    Update print_integer.c

--- print_integer.c
+++ print_integer.c
@@ -1,40 +1,25 @@
 #include "main.h"
 
 /**
- *print_integer - prints an integer
- *@n: integer to be printed
+ *print_integer - Prints an integer to stdout
+ *@n: The integer to print
  *
- *Return: number of digits printed
+ *Return: The number of digits printed
  */
-int print_integer(long int n)
+int print_integer(int n)
 {
-	unsigned int num, last_digit;
-	int digit_count = 0;
+	int count = 0;
 
-	if (n == INT_MIN)
+	if (n < 0)
 	{
-		num = (unsigned int) - (n / 10);
-		last_digit = (unsigned int) - (n % 10);
-		_putchar('-');
-		digit_count++;
-		print_unsigned(num, 10, 0);
-		digit_count += count_digits(num, 10);
-		_putchar(last_digit + '0');
-		digit_count++;
-	}
-	else
-	{
-		if (n < 0)
-		{
-			_putchar('-');
-			digit_count++;
-			num = (unsigned int) - n;
-		}
-		else
-			num = (unsigned int) n;
-
-		digit_count += print_unsigned(num, 10, 0);
+		count += _putchar('-');
+		n = -n;
 	}
 
-	return (digit_count);
+	if (n / 10 != 0)
+		count += print_integer(n / 10);
+
+	count += _putchar((n % 10) + '0');
+
+	return (count);
 }
