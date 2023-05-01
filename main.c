640d46e215f261c79b486689f51b69ceb0faa04c
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-01 23:51:17 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,32 +1,36 @@
 #include "main.h"
+#include <stdio.h>
+
 
 /**
  *main - Entry point
  *
  *Return: Always 0
  */
-#include <stdio.h>
+int main(void)
+{
+    int len;
+    char *str;
 
-int main() {
-    printf("Test case 1:\n");
-    printf("Expected output: No special character.No special character.\n");
-    _printf("%S", "No special character.");
-    
-    printf("\nTest case 2:\n");
-    printf("Expected output: \\x0A\\x0A\n");
-    _printf("%S", "\n");
-    
-    printf("\nTest case 3:\n");
-    printf("Expected output: \\x01\\x02\\x03\\x04\\x05\\x06\\x07\\x01\\x02\\x03\\x04\\x05\\x06\\x07\n");
-    _printf("%S", "\x01\x02\x03\x04\x05\x06\x07");
-    
-    printf("\nTest case 4:\n");
-    printf("Expected output: Could you print some non-prntable characters?\nSure:\\x1F\\x7F\\x0A\nThanks!\n");
-    _printf("Could you print some non-prntable characters?\n%S\nThanks!\n", "Sure:\x1F\x7F\n");
-    
-    printf("\nTest case 5:\n");
-    printf("Expected output: - What did you say?\n- \n- That's what I thought.\n");
-    _printf("- What did you say?\n- %S\n- That's what I thought.\n", "");
+    len = _printf("%S", "No special character.");
+    str = "No special character.No special character.";
+    if (len != 42 || strcmp(_buffer, str) != 0)
+        printf("Error: case 1\nGot: [%.*s]\nExpected: [%s]\n", len, _buffer, str);
 
-    return 0;
+    len = _printf("%S", "\n");
+    str = "\x0A\x0A";
+    if (len != 8 || strcmp(_buffer, str) != 0)
+        printf("Error: case 2\nGot: [%.*s]\nExpected: [%s]\n", len, _buffer, str);
+
+    len = _printf("%S", "\x01\x02\x03\x04\x05\x06\x07");
+    str = "\x01\x02\x03\x04\x05\x06\x07\x01\x02\x03\x04\x05\x06\x07";
+    if (len != 56 || strcmp(_buffer, str) != 0)
+        printf("Error: case 3\nGot: [%.*s]\nExpected: [%s]\n", len, _buffer, str);
+
+    len = _printf("Could you print some non-prntable characters?\n%S\nThanks!\n", "Sure:\x1F\x7F\n");
+    str = "Could you print some non-prntable characters?\nSure:\x1F\x7F\x0A\nThanks!\n";
+    if (len != 144 || strcmp(_buffer, str) != 0)
+        printf("Error: case 4\nGot: [%.*s]\nExpected: [%s]\n", len, _buffer, str);
+
+    return (0);
 }
