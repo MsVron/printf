5aa01ce5f01e58c76248b445f70ee7ec11817d14
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-01 02:17:51 +0100

    Update print_string.c

--- print_string.c
+++ print_string.c
@@ -1,24 +1,31 @@
 #include "main.h"
 
 /**
- *print_string - prints a string
- *@arg: the argument to print
- *Return: the number of characters printed
+ *print_string - prints a string, replacing non-printable
+ *               characters with escape sequences
+ *@args: arguments list containing the string to print
+ *@buf: buffer to print to
+ *@pos: current position in the buffer
+ *
+ *Return: the new position in the buffer
  */
-
-int print_string(va_list arg)
+int print_string(va_list args, char *buf, int pos)
 {
-	char *str = va_arg(arg, char *);
-	int len = 0;
+	char *str = va_arg(args, char *);
+	int i, len;
 
 	if (str == NULL)
 		str = "(null)";
 
-	while (str[len])
+	len = _strlen(str);
+
+	for (i = 0; i < len; i++)
 	{
-		_putchar(str[len]);
-		len++;
+		if (str[i] < 32 || str[i] >= 127)
+			pos += _strcpy(buf + pos, "\\x"), pos += _to_hex(buf + pos, str[i]);
+		else
+			buf[pos++] = str[i];
 	}
 
-	return (len);
+	return (pos);
 }
