3a23a4ec948d36d8d4d1444ec1df51d701b78460
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-01 02:23:17 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <limits.h>
-#include <stdio.h>
 
 /**
  *main - Entry point
@@ -9,41 +7,17 @@
  */
 int main(void)
 {
-    char *str1 = "No special character.";
-    char *str2 = "\n";
-    char *str3 = "\x01\x02\x03\x04\x05\x06\x07";
-    char *str4 = "Sure:\x1F\x7F\n";
-    char *str5 = "";
+    char *str1 = "hello, world!";
+    char *str2 = "This is a string.";
+    char *str3 = "This is another string.";
+    char *str4 = "The value of pi is approximately 3.14159265359.";
+    char *str5 = "The quick brown fox jumps over the lazy dog.";
 
-    printf("Testing case: _printf(\"%%S\", \"No special character.\");\n");
-    printf("Expected output: No special character.\n");
-    printf("Got output: ");
-    print_string(str1);
-    printf("\n\n");
+    _printf("%s\n", str1);
+    _printf("%s\n", str2);
+    _printf("%s\n", str3);
+    _printf("%s\n", str4);
+    _printf("%s\n", str5);
 
-    printf("Testing case: _printf(\"%%S\", \"\\n\");\n");
-    printf("Expected output: \\x0A\\x0A\n");
-    printf("Got output: ");
-    print_string(str2);
-    printf("\n\n");
-
-    printf("Testing case: _printf(\"%%S\", \"\\x01\\x02\\x03\\x04\\x05\\x06\\x07\");\n");
-    printf("Expected output: \\x01\\x02\\x03\\x04\\x05\\x06\\x07\\x01\\x02\\x03\\x04\\x05\\x06\\x07\n");
-    printf("Got output: ");
-    print_string(str3);
-    printf("\n\n");
-
-    printf("Testing case: _printf(\"Could you print some non-prntable characters?\\n%%S\\nThanks!\\n\", \"Sure:\\x1F\\x7F\\n\");\n");
-    printf("Expected output: Could you print some non-prntable characters?\nSure:\\x1F\\x7F\\x0A\nThanks!\n");
-    printf("Got output: ");
-    print_string(str4);
-    printf("\n\n");
-
-    printf("Testing case: _printf(\"- What did you say?\\n- %%S\\n- That's what I thought.\\n\", \"\");\n");
-    printf("Expected output: - What did you say?\n-\n- That's what I thought.\n");
-    printf("Got output: ");
-    print_string(str5);
-    printf("\n\n");
-
-    return (0);
+    return 0;
 }
