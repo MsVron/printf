a66f6ea2b49c8d688914330f7e6990a3b36d3942
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-01 02:26:31 +0100

    Update main.h

--- main.h
+++ main.h
@@ -14,5 +14,8 @@ int print_integer(int n);
 int print_binary(unsigned int n);
 int print_unsigned(unsigned int n, int base, int uppercase);
 int print_string(va_list args, char *buf, int pos);
+int _to_hex(char *buf, unsigned int num);
+int _strcpy(char *dest, char *src);
+int _strlen(char *str);
 
 #endif /* MAIN_H */
